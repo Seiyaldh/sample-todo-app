2ca73303b5d0237f5611273bf2e1d0683ca24af3
nakasei <e175711@ie.u-ryukyu.ac.jp>
2021-02-27 18:33:31 +0900

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 /* このファイルに記述されているSQLは起動時に実行されます */
 
 INSERT INTO TODO_APP VALUES (1, 'Housekeeping', 'Bathroom, WC', '2019-09-07');
-INSERT INTO TODO_APP VALUES (2, 'Buy present', 'Present for birthday.', '2019-09-07');
+INSERT INTO TODO_APP VALUES (2, 'Buy present', 'Present for birthday.', '2019-09-01');
