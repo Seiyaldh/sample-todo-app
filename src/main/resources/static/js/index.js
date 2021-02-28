aece99469ebbd1dab02e55ffe6c0c9aa056825d4
nakasei <e175711@ie.u-ryukyu.ac.jp>
2021-03-01 03:13:36 +0900

    add index.js

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,15 @@
+//削除チェックの関数
+function releaseCheck() {
+    // 「OK」時の処理開始 ＋ 確認ダイアログの表示
+	if(window.confirm('本当にいいんですか？')){
+		return true;
+	}
+	// 「OK」時の処理終了
+
+	// 「キャンセル」時の処理開始
+	else{
+		window.alert('キャンセルされました'); // 警告ダイアログを表示
+        return false;
+	}
+	// 「キャンセル」時の処理終了
+}
