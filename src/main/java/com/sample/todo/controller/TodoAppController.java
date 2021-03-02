22680d3799f274f129157d81010462043058a462
nakasei <e175711@ie.u-ryukyu.ac.jp>
2021-03-02 15:31:23 +0900

    fix controller

--- src/main/java/com/sample/todo/controller/TodoAppController.java
+++ src/main/java/com/sample/todo/controller/TodoAppController.java
@@ -27,31 +27,34 @@ public class TodoAppController {
 
     @Autowired
     private TodoAppService service;
-    
-    //エラーメッセージを取得
+
+    // エラーメッセージを取得
     @Autowired
     private MessageSource msg;
 
+    // registerとupdaterのエラーリストを取得
     public static List<String> getErrorList(BindingResult result, MessageSource msg) {
         List<String> errorList = new ArrayList<String>();
-            for(ObjectError error : result.getAllErrors()) {
-                if (error.getDefaultMessage().matches(".*(dueDate).*")){
-                    String message = msg.getMessage("date_error_key", null, Locale.JAPAN);
-                    errorList.add(message);//もしdueDateでエラーが起きたら自作のエラーメッセージをadd
-                } else {
-                    errorList.add(error.getDefaultMessage());
-                }
+        for (ObjectError error : result.getAllErrors()) {
+            if (error.getDefaultMessage().matches(".*(dueDate).*")) {
+                String message = msg.getMessage("date_error_key", null, Locale.JAPAN);
+                errorList.add(message);// もしdueDateでエラーが起きたら自作のエラーメッセージをadd
+            } else {
+                errorList.add(error.getDefaultMessage());
             }
+        }
         return errorList;
     }
 
-    public static String valEmptyTodoList(List<TodoApp> todoList, MessageSource msg, TodoAppService service, Model model) {
+    // 検索時の空検証と結果に応じたリストとエラーメッセージの取得
+    public static String valEmptyTodoList(List<TodoApp> todoList, MessageSource msg, TodoAppService service,
+            Model model) {
         List<TodoApp> mostRecentTodoList = service.getMostRecentTodoAppList();
         model.addAttribute("mostRecentTodoList", mostRecentTodoList);
-        if (todoList.size()!=0){
+        if (todoList.size() != 0) {
             model.addAttribute("todoList", todoList);// ここの"todoList"というキーがindex.htmlで参照されている
-            return "index";// もしListが検索されたら検索結果を渡す
-        }
+            return "index";
+        }// もしListが検索されたら検索結果を渡す
         todoList = service.getTodoAppList();
         model.addAttribute("todoList", todoList);
         String noHitMsg = msg.getMessage("no_hit", null, Locale.JAPAN);
@@ -59,8 +62,8 @@ public class TodoAppController {
         return "index";
     }
 
-    //update.htmlのパラメーターを設定
-    public static void addParmUpdate(int todoId, String title, String detail, String dueDate, Model model){
+    // update.htmlへのパラメーターを設定
+    public static void addParmUpdate(int todoId, String title, String detail, String dueDate, Model model) {
         model.addAttribute("todoId", todoId);
         model.addAttribute("title", title);
         model.addAttribute("detail", detail);
@@ -82,74 +85,80 @@ public class TodoAppController {
     }
 
     @RequestMapping(value = "/new", method = { RequestMethod.GET, RequestMethod.POST })
-    String add(@Validated @ModelAttribute TodoApp todoApp, BindingResult result, Model model) {
+    String add(@ModelAttribute TodoApp todoApp, Model model) {
         return "detail";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    String register(@Validated @ModelAttribute TodoApp todoApp, BindingResult result, Model model) {
+    String register(@RequestParam String title, String detail, String dueDate,
+            @Validated @ModelAttribute TodoApp todoApp, BindingResult result, Model model) {
         if (result.hasErrors()) {
             List<String> errorList = getErrorList(result, msg);
+            model.addAttribute("title", title);
+            model.addAttribute("detail", detail);
+            model.addAttribute("dueDate", dueDate);
             model.addAttribute("validationError", errorList);
             return "detail";
-        }//もしエラーが吐かれたらエラーメッセージを表示しdetailに戻る
+        } // もしエラーが吐かれたらエラーメッセージを表示しdetailに戻る
         service.register(todoApp.getTitle(), todoApp.getDetail(), todoApp.getDueDate());
         return "redirect:index";
     }
 
-    //解除の指示を行う
+    // 解除の指示を行う
     @RequestMapping(value = "/release", method = { RequestMethod.GET, RequestMethod.POST })
     String release(@RequestParam int releaseId, Model model) {
         service.release(releaseId);
         return "redirect:index";
     }
 
-    @RequestMapping(value = "/change", method =  {RequestMethod.GET, RequestMethod.POST })
+    @RequestMapping(value = "/change", method = { RequestMethod.GET, RequestMethod.POST })
     String change(@RequestParam int todoId, String title, String detail, String dueDate, Model model) {
         addParmUpdate(todoId, title, detail, dueDate, model);
         return "update";
     }
 
     @RequestMapping(value = "/updater", method = { RequestMethod.GET, RequestMethod.POST })
-    String updater(@RequestParam int todoId, String title, String detail, String dueDate, @Validated @ModelAttribute TodoApp todoApp, BindingResult result, Model model) {
+    String updater(@RequestParam int todoId, String title, String detail, String dueDate,
+            @Validated @ModelAttribute TodoApp todoApp, BindingResult result, Model model) {
         if (result.hasErrors()) {
             List<String> errorList = getErrorList(result, msg);
-            model.addAttribute("validationError", errorList);         
+            model.addAttribute("validationError", errorList);
             addParmUpdate(todoId, title, detail, dueDate, model);
             return "update";
-        }
+        } // もしエラーが吐かれたらエラーメッセージを表示しupdateに戻る
         service.updater(todoId, todoApp.getTitle(), todoApp.getDetail(), todoApp.getDueDate());
         return "redirect:index";
     }
 
-    @RequestMapping(value = "/searchTitle" , method = { RequestMethod.GET, RequestMethod.POST })
+    @RequestMapping(value = "/searchTitle", method = { RequestMethod.GET, RequestMethod.POST })
     String searchTitle(@ModelAttribute TodoApp todoApp, Model model) {
         List<TodoApp> todoList = service.getSearchTitle(todoApp.getTitle());
         return valEmptyTodoList(todoList, msg, service, model);
     }
 
-    @RequestMapping(value = "/searchDetail" , method = { RequestMethod.GET, RequestMethod.POST })
+    @RequestMapping(value = "/searchDetail", method = { RequestMethod.GET, RequestMethod.POST })
     String searchDetail(@ModelAttribute TodoApp todoApp, Model model) {
         List<TodoApp> todoList = service.getSearchDetail(todoApp.getDetail());
         return valEmptyTodoList(todoList, msg, service, model);
     }
 
-    @RequestMapping(value = "/searchDueDate" , method = { RequestMethod.GET, RequestMethod.POST })
+    @RequestMapping(value = "/searchDueDate", method = { RequestMethod.GET, RequestMethod.POST })
     String searchDueDate(@Validated @ModelAttribute TodoApp todoApp, BindingResult result, Model model) {
-        List<TodoApp> todoList = service.getSearchDueDate(todoApp.getDueDate(),todoApp.getStartDueDate());
+        List<TodoApp> todoList = service.getSearchDueDate(todoApp.getDueDate(), todoApp.getStartDueDate());
         if (result.hasErrors()) {
-            for(ObjectError error : result.getAllErrors()) {
-                if (error.getDefaultMessage().matches(".*(dueDate).*") || error.getDefaultMessage().matches(".*(startDueDate).*")) {
+            for (ObjectError error : result.getAllErrors()) {
+                if (error.getDefaultMessage().matches(".*(dueDate).*")
+                        || error.getDefaultMessage().matches(".*(startDueDate).*")) {
                     todoList = service.getTodoAppList();
                     model.addAttribute("todoList", todoList);
-                    String noHitMsg = msg.getMessage("date_error_key", null, Locale.JAPAN);//もしdueDateでエラーが起きたら自作のエラーメッセージを取得
+                    String noHitMsg = msg.getMessage("date_error_key", null, Locale.JAPAN);// dueDate関連でエラーが起きたら自作のエラーメッセージを取得
                     model.addAttribute("noHitMsg", noHitMsg);
                     List<TodoApp> mostRecentTodoList = service.getMostRecentTodoAppList();
                     model.addAttribute("mostRecentTodoList", mostRecentTodoList);
                     return "index";
                 }
             }
-        }
+        } // もしエラーが吐かれたらエラーメッセージを表示しindexに戻る
         return valEmptyTodoList(todoList, msg, service, model);
     }
 }
